5fbdb6307bb212e85b636c9b8fdd127c4387941b
Velizar Ganchev <velizar_ganchev@hotmail.com>
2025-12-21 15:37:32 +0100

    initial commit

---
+++ Controllers/PlaceController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PremiumPlace_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlacesController : ControllerBase
+    {
+        [HttpGet]
+        public string GetPlaces()
+        {
+            return "List of places";
+        }
+
+        [HttpGet("{id:int}")]
+        public string GetPlaceById(int id)
+        {
+            return $"Get Place: {id}";
+        }
+    }
+}
